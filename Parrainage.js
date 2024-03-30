2e850442cbd1b8995ebb9f8f03036eb076783cdb
Marie Solange Diouf <164958064+Solange883@users.noreply.github.com>
2024-03-30 13:47:37 +0000

    Add files via upload

---
+++ Parrainage.js
@@ -0,0 +1,17 @@
+
+const parrainer= new Set();
+
+function Envoyer() { 
+    const nom= document.getElementById('nom').value;
+    const cni= document.getElementById('cni').value;
+    const selectedCandidate = document.getElementById('candidat').value;
+    if (!parrainer.has(cni)) {
+        parrainer.add(cni);
+        alert( `Merci,   ${nom}! Vous avez parrainé pour ${selectedCandidate}.`);
+    } else {
+        alert(`Désolé, ${nom}.Vous avez déjà parrainé.`);
+}
+
+  
+}
+
